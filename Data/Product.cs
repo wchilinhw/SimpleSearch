a890383876e757b96210385438342cec2914b478
wchilinhw <wchilinhw@users.noreply.github.com>
2021-03-04 11:25:41 +0700

    Hoàn thành chức năng tìm kiếm theo từ khóa đơn giản (Data tạo sẵn, không tương tác CSDL)

---
+++ Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_SimpleSearch.Data
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_SimpleSearch.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected string SearchTerm { get; set; }
+        List<Data.Product> Products { get; set; } = new List<Data.Product>
+{
+new Data.Product {Title = "Smart speaker", Price = 22m, Image =
+"test-image.png"},
+new Data.Product {Title = "Dumb speaker", Price = 89m, Image =
+"test-image.png"},
+new Data.Product {Title = "Speaker cable", Price = 12m, Image =
+"test-image.png"},
+new Data.Product {Title = "T-Shirt", Price = 32m, Image = "test-image.png"},
+new Data.Product {Title = "Large TV", Price = 119.90m, Image =
+"test-image.png"},
+};
+        //Tim kiem if
+        //      null => lay het du lieu ra
+        //      co chua string => lay theo string do
+        protected IEnumerable<Data.Product> SearchResults => Products.Where(p => string.IsNullOrEmpty(SearchTerm)
+         || p.Title.ToLower().Contains(SearchTerm.ToLower()));
+
+
+    }
+
+}
